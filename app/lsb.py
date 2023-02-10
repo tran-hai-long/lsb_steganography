8731598c98e961e296f1235d751e18c95bb1c7a8
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-11 02:56:18 +0700

    Create placeholder templates

--- app/lsb.py
+++ app/lsb.py
@@ -6,3 +6,13 @@ bp = Blueprint("lsb", __name__, url_prefix="/lsb")
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/encode/")
+def encode():
+    return render_template("encode.html")
+
+
+@bp.route("/decode/")
+def decode():
+    return render_template("decode.html")
