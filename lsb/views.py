2a0b805b3829660ea854acb1e64a7d6a0bd6e232
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-11 22:47:48 +0700

    Setup placeholder encode() and decode() functions

--- lsb/views.py
+++ lsb/views.py
@@ -1,8 +1,7 @@
+from flask import Blueprint, request
 from flask import render_template
 
 from lsb.forms import EncodeForm, DecodeForm
-
-from flask import Blueprint
 
 bp_lsb = Blueprint("lsb", __name__, url_prefix="/lsb", template_folder="templates")
 
@@ -12,13 +11,25 @@ def index():
     return render_template("index.html")
 
 
-@bp_lsb.route("/encode/")
+@bp_lsb.route("/encode/", methods=["GET", "POST"])
 def encode_page():
     form = EncodeForm()
+    if request.method == "POST" and form.validate_on_submit():
+        encode(form.message.data, form.image.data)
     return render_template("encode.html", form=form)
 
 
-@bp_lsb.route("/decode/")
+@bp_lsb.route("/decode/", methods=["GET", "POST"])
 def decode_page():
     form = DecodeForm()
+    if request.method == "POST" and form.validate_on_submit():
+        decode(form.image.data)
     return render_template("decode.html", form=form)
+
+
+def encode(message, image):
+    pass
+
+
+def decode(image):
+    pass
