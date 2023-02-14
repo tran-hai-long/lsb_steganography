2082da913fd69661bb71c0c01e37d295a9a8981c
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-15 02:31:54 +0700

    Add some error messages in forms.py

--- lsb/forms.py
+++ lsb/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, AnyOf
 
 
 class EncodeForm(FlaskForm):
-    message = StringField("Message", validators=[DataRequired()])
-    image = FileField("Image", validators=[FileRequired(), FileAllowed(["png"], "PNG image required.")])
+    message = StringField("Message", validators=[DataRequired("Message required.")])
+    image = FileField("Image", validators=[FileRequired("Image required."), FileAllowed(["png"], "PNG image required.")])
     consumed_bits = RadioField("How many bits per color channel to be used for encoding?",
                                choices=[("1", "1bpc"), ("2", "2bpc"), ("4", "4bpc")],
                                validators=[AnyOf(["1", "2", "4"], "Please choose one of the radio buttons.")])
@@ -14,8 +14,8 @@ class EncodeForm(FlaskForm):
 
 
 class DecodeForm(FlaskForm):
-    image = FileField("Image", validators=[FileRequired(), FileAllowed(["png"], "PNG image required.")])
+    image = FileField("Image", validators=[FileRequired("Image required."), FileAllowed(["png"], "PNG image required.")])
     consumed_bits = RadioField("How many bits per color channel were used during the encoding process?",
-                               choices=[(1, "1bpc"), (2, "2bpc"), (4, "4bpc")],
+                               choices=[("1", "1bpc"), ("2", "2bpc"), ("4", "4bpc")],
                                validators=[AnyOf(["1", "2", "4"], "Please choose one of the radio buttons.")])
     submit = SubmitField("Submit")
