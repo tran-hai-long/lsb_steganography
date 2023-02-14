f76fb47435214429b9dd5fd055191ef86236c9b1
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-14 22:59:08 +0700

    Replace HTML button with WTForms SubmitField

--- lsb/forms.py
+++ lsb/forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class EncodeForm(FlaskForm):
     message = StringField("Message", validators=[DataRequired()])
     image = FileField("Image", validators=[FileRequired(), FileAllowed(["png"], "PNG image required.")])
+    submit = SubmitField("Submit")
 
 
 class DecodeForm(FlaskForm):
     image = FileField("Image", validators=[FileRequired(), FileAllowed(["png"], "PNG image required.")])
+    submit = SubmitField("Submit")
