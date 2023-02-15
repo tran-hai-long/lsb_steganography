dd9ea44dfd7fbcfe167d0802bd81277bf4d9d59d
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-15 14:09:40 +0700

    Rename variables in EncodeFormTest

--- tests/lsb/test_forms.py
+++ tests/lsb/test_forms.py
@@ -7,36 +7,37 @@ from lsb.forms import EncodeForm
 
 class EncodeFormTest(TestCase):
     def setUp(self) -> None:
-        self.app = create_app()
-        self.appcontext = self.app.app_context()
-        self.appcontext.push()
-        self.request_context = self.app.test_request_context()
-        self.request_context.push()
+        self.test_app = create_app()
+        self.test_app.config["TESTING"] = True
+        self.test_app_context = self.test_app.app_context()
+        self.test_app_context.push()
+        self.test_request_context = self.test_app.test_request_context()
+        self.test_request_context.push()
 
     def tearDown(self) -> None:
-        self.appcontext.pop()
-        self.request_context.pop()
-        self.app = None
-        self.appcontext = None
-        self.request_context = None
+        self.test_app_context.pop()
+        self.test_request_context.pop()
+        self.test_app = None
+        self.test_app_context = None
+        self.test_request_context = None
 
     def test_message_label(self):
-        with self.appcontext and self.request_context:
+        with self.test_app_context and self.test_request_context:
             form = EncodeForm()
             self.assertEqual(form.message.label.text, "Message")
 
     def test_image_label(self):
-        with self.appcontext and self.request_context:
+        with self.test_app_context and self.test_request_context:
             form = EncodeForm()
             self.assertEqual(form.image.label.text, "Image")
 
     def test_consumed_bits_label(self):
-        with self.appcontext and self.request_context:
+        with self.test_app_context and self.test_request_context:
             form = EncodeForm()
             self.assertEqual(form.consumed_bits.label.text, "How many bits per color channel to be used for encoding?")
 
     def test_submit_label(self):
-        with self.appcontext and self.request_context:
+        with self.test_app_context and self.test_request_context:
             form = EncodeForm()
             self.assertEqual(form.submit.label.text, "Submit")
 
