6bd1c609ae3248e0e7234f1abf6dc38953a6fac4
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-02-19 14:14:02 +0700

    Make form RadioField default to 1

--- lsb/forms.py
+++ lsb/forms.py
@@ -12,6 +12,7 @@ class EncodeForm(FlaskForm):
     consumed_bits = RadioField(
         "How many bits per color channel to be used for encoding?",
         choices=[("1", "1bpc"), ("2", "2bpc"), ("4", "4bpc")],
+        default="1",
         validators=[AnyOf(["1", "2", "4"], "Please choose one of the radio buttons.")],
     )
     submit = SubmitField("Submit")
@@ -24,6 +25,7 @@ class DecodeForm(FlaskForm):
     consumed_bits = RadioField(
         "How many bits per color channel were used during the encoding process?",
         choices=[("1", "1bpc"), ("2", "2bpc"), ("4", "4bpc")],
+        default="1",
         validators=[AnyOf(["1", "2", "4"], "Please choose one of the radio buttons.")],
     )
     submit = SubmitField("Submit")
