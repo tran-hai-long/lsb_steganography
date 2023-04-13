35ebecb99705a92d36da4f646c3d51fbd66dac4a
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-04-14 00:51:07 +0700

    Move template and static files to root folder

--- lsb/views.py
+++ lsb/views.py
@@ -5,9 +5,7 @@ from flask import render_template
 from .bin_ascii import ascii_str_to_bin
 from .forms import EncodeForm, DecodeForm
 
-bp_lsb = Blueprint(
-    "lsb", __name__, url_prefix="/lsb", template_folder="templates", static_folder="static"
-)
+bp_lsb = Blueprint("lsb", __name__, url_prefix="/lsb")
 
 STARTER: str = "#start#"
 STARTER_LENGTH: int = len(STARTER)
@@ -40,11 +38,11 @@ def index():
 def encode_page():
     form = EncodeForm()
     if request.method != "POST" or not form.validate_on_submit():
-        return render_template("encode.html", form=form)
+        return render_template("lsb-encode.html", form=form)
     if not verify_ascii(form.message.data):
-        return render_template("encode.html", form=form, error="ASCII characters only.")
+        return render_template("lsb-encode.html", form=form, error="ASCII characters only.")
     if not verify_png_jpeg(form.image.data):
-        return render_template("encode.html", form=form, error="PNG images only.")
+        return render_template("lsb-encode.html", form=form, error="PNG images only.")
     # Delimiter is used to signal the end of message
     msg_with_starter_and_delimiter: str = STARTER + form.message.data + DELIMITER
     bin_msg_with_starter_and_delimiter: str = ascii_str_to_bin(msg_with_starter_and_delimiter)
@@ -52,33 +50,33 @@ def encode_page():
     image: Image = Image.open(form.image.data)
     channel: int = verify_channel(image)
     if not channel:
-        return render_template("encode.html", form=form, error="RGB or RGBA color channel only.")
+        return render_template("lsb-encode.html", form=form, error="RGB or RGBA color channel only.")
     if not check_if_msg_fit_in_img(
         bin_msg_with_starter_and_delimiter, image, channel, consumed_bits
     ):
         return render_template(
-            "encode.html", form=form, error="The message does not fit in the image."
+            "lsb-encode.html", form=form, error="The message does not fit in the image."
         )
     result: Image = encode(bin_msg_with_starter_and_delimiter, image, consumed_bits)
     # Pillow Image objects can not be displayed in HTML, thus it is necessary to convert it to base64
     result_base64: str = buffer_and_convert_b64(result)
-    return render_template("encode.html", form=form, result=result_base64)
+    return render_template("lsb-encode.html", form=form, result=result_base64)
 
 
 @bp_lsb.route("/decode/", methods=["GET", "POST"])
 def decode_page():
     form = DecodeForm()
     if request.method != "POST" or not form.validate_on_submit():
-        return render_template("decode.html", form=form)
+        return render_template("lsb-decode.html", form=form)
     if not verify_png(form.image.data):
-        return render_template("decode.html", form=form, error="PNG images only.")
+        return render_template("lsb-decode.html", form=form, error="PNG images only.")
     consumed_bits: int = int(form.consumed_bits.data)
     image: Image = Image.open(form.image.data)
     channel: int = verify_channel(image)
     if not channel:
-        return render_template("decode.html", form=form, error="RGB or RGBA color channel only.")
+        return render_template("lsb-decode.html", form=form, error="RGB or RGBA color channel only.")
     result: str = decode(image, consumed_bits)
-    return render_template("decode.html", form=form, result=result)
+    return render_template("lsb-decode.html", form=form, result=result)
 
 
 @bp_lsb.route("/explain/")
